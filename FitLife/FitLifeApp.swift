f2a997d98d968120a4c287dafe2984a81930ddba
Alandis <1seals.alandis@gmail.com>
2022-07-18 18:04:58 -0500

    first commit

---
+++ FitLife/FitLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitLifeApp.swift
+//  FitLife
+//
+//  Created by Alandis Seals on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct FitLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
