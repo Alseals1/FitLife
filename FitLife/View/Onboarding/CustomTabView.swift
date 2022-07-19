68f8faf55e35f855806fd58b1177f3f4f53fcb91
Alandis <1seals.alandis@gmail.com>
2022-07-19 08:45:19 -0500

    Finish Onboarding Views

--- FitLife/View/Onboarding/CustomTabView.swift
+++ FitLife/View/Onboarding/CustomTabView.swift
@@ -4,11 +4,12 @@ struct CustomTabView: View {
     var body: some View {
         TabView {
             OnboardingView(title: "Workout From Home", image: "barbell-image",
-                           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan, sollicitudin ornare cras ut ac. Neque amet, at gravida             vel nibh gravida.")
+                           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan, sollicitudin ornare cras ut ac. Neque amet, at gravidavel nibh gravida.")
             
             
             OnboardingView(title: "Choose us, Choose health.", image: "heartbeat-image",
                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan, sollicitudin ornare cras ut ac. Neque amet, at gravida vel nibh gravida.")
+            Verification()
         }
         .tabViewStyle(.page(indexDisplayMode: .always))
         .indexViewStyle(.page(backgroundDisplayMode: .always))
