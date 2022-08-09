714ddc67a4093a054e250daf381fa51f96c688ab
Alandis <1seals.alandis@gmail.com>
2022-08-09 18:44:34 -0500

    Added Tab View

--- FitLife/View/Home/TabsView.swift
+++ FitLife/View/Home/TabsView.swift
@@ -1,8 +1,35 @@
 import SwiftUI
 
 struct TabsView: View {
+    @State var selectedTab: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selectedTab) {
+           HomeView()
+                .tabItem {
+                    Image("home")
+                    Text("Home")
+                        
+                }.tag(0)
+            
+            Text("Search")
+                .tabItem {
+                    Image("search")
+                    Text("Search")
+                }.tag(1)
+            
+            Text("Article")
+                .tabItem {
+                    Image("document-text")
+                    Text("Article")
+                }
+            
+          TrainerProfileView()
+                .tabItem {
+                    Image("user")
+                    Text("Profile")
+                }
+        }
+        .accentColor(.buttonorange)
     }
 }
 
